cf5853f1ba29bc58901391706274253d31a43c3d
KirillDolbnya <kirilldolbnya6@gmail.com>
2024-03-02 18:06:05 +0300

    routs

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,62 +17,24 @@ use Illuminate\Support\Facades\Storage;
 |
 */
 
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::controller(ImageController::class)->group(function (){
+        Route::get('/dashboard', 'index')->name('dashboard.index');
+        Route::get('/create', 'create')->name('create');
+        Route::get('/show/{id}', 'show')->name('show');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/dashboard/{id}', 'edit')->name('edit');
+        Route::put('/dashboard/{id}', 'update')->name('update');
+        Route::delete('/dashboard/{id}', 'delete')->name('delete');
+    });
+});
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    $images = DB::table('images')->select('*')->get();
-    $myImages =  $images->all();
 
-    return view('dashboard', ['ImagesInView' => $myImages]);
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/create', function () {
-    return view('create');
-})->middleware(['auth', 'verified'])->name('create');
-
-Route::get('/show/{id}', function ($id) {
-    $image = DB::table('images')->select('*')->where('id', $id)->first();
-    $myImage = $image->image;
-    return view('show', ['ImageInView' => $myImage]);
-})->middleware(['auth', 'verified'])->name('show');
-
-Route::post('/store', function (\Illuminate\Http\Request $request){
-    $image = $request->file('image');
-    $fileName = $image->store('uploads');
-    DB::table('images')->insert(
-        ['image' => $fileName]
-    );
-    return redirect('/');
-});
-
-Route::get('/edit/{id}', function ($id) {
-    $image = DB::table('images')->select('*')->where('id', $id)->first();
-    return view('edit',['ImageInView' => $image]);
-})->middleware(['auth', 'verified'])->name('edit');
-
-Route::post('/update/{id}', function (\Illuminate\Http\Request $request,$id){
-    $image = DB::table('images')->select('*')->where('id', $id)->first();
-    $deleteImage = $image->image;
-    Storage::delete($deleteImage);
-
-    $filename = $request->image->store('uploads');
-    DB::table('images')
-        ->where('id',$id)
-        ->update(['image' => $filename]);
-    return redirect('/');
-});
-
-Route::get('/delete/{id}', function ($id){
-    $image = DB::table('images')->select('*')->where('id', $id)->first();
-    $deleteImage = $image->image;
-    Storage::delete($deleteImage);
-
-    DB::table('images')->where('id', $id)->delete();
-
-    return redirect('/');
-});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
